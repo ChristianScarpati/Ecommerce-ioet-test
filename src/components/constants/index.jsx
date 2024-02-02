78e24c42fbb15d77496fb56e89640618bce2f844
ChristianScarpati <christian-mallmann@hotmail.com>
2024-02-02 15:41:15 -0300

    fix filters by rate

--- src/components/constants/index.jsx
+++ src/components/constants/index.jsx
@@ -2,3 +2,4 @@ export const PRICE_LOW = "Price_Low";
 export const PRICE_HIGH = "Price_High";
 export const NAME = "Name";
 export const GET_PRODUCTS_API = "https://fakestoreapi.com/products";
+export const RATE_OPTIONS = [5, 4, 3, 2, 1]
